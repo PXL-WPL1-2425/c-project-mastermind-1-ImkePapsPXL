7981525da287a5073aafbd5aeace29cfcc2d4b34
ImkePapsPXL <Imke.Paps@student.pxl.be>
2024-11-04 15:36:55 +0100

    MasterMind-02: Code

--- mastermind/mastermind/MainWindow.xaml.cs
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,10 +20,24 @@ namespace mastermind
         public MainWindow()
         {
             InitializeComponent();
+
+            Random random = new Random();
+            SolidColorBrush[] colorCode = { Brushes.Red, Brushes.Green, Brushes.White, Brushes.Yellow, Brushes.Blue, Brushes.Orange };
+            int[] randomColors = new int[4];
+            string[] colorName = {"red", "Green", "White", "Yellow", "Blue", "Orange"};
+
+            for (int i = 0; i < randomColors.Length; i++)
+            {
+                int index = random.Next(colorCode.Length);
+                randomColors[i] = random.Next(1, 6);
+            }
+
+            this.Title = $"{colorName[randomColors[0]].ToString()}, {colorName[randomColors[1]].ToString()}, {colorName[randomColors[2]].ToString()}, {colorName[randomColors[3]].ToString()}";
+         
         }
 
         
-        private void firstCodeBlueButton_Checked(object sender, RoutedEventArgs e)
+    private void firstCodeBlueButton_Checked(object sender, RoutedEventArgs e)
         {
 
         }
@@ -48,6 +63,104 @@ namespace mastermind
         }
 
         private void firstCodeGreenButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        
+
+
+        private void secondCodeBlueButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void secondCodeRedButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void secondCodeYellowButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void secondCodeWhiteButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void secondCodeOrangeButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void secondCodeGreenButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirdCodeBlueButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirdCodeRedButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirdCodeYellowButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirdCodeWhiteButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirdCodeOrangeButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void thirdCodeGreenButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourthCodeBlueButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourthCodeRedButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourthCodeYellowButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourthCodeWhiteButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourthtCodeOrangeButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fourthCodeGreenButton_Checked(object sender, RoutedEventArgs e)
         {
 
         }
