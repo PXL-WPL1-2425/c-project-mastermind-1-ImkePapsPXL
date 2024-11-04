2be469091c4812a023b1600a887450e0a89887e7
ImkePapsPXL <Imke.Paps@student.pxl.be>
2024-11-04 14:32:15 +0100

    Mastermind-01: Opstarten

---
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mastermind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void firstCodeBlueButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void firstCodeRedButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void firstCodeYellowButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void firstCodeWhiteButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void firstCodeOrangeButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void firstCodeGreenButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
