0a1303d1184a0b1a495ef724dc157dd2920d70ec
ImkePapsPXL <Imke.Paps@student.pxl.be>
2024-11-16 15:52:57 +0100

    Mastermind-02: Code

--- mastermind/mastermind/MainWindow.xaml.cs
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -17,14 +17,28 @@ namespace mastermind
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        Random random = new Random();
+        SolidColorBrush[] colorCode = { Brushes.Red, Brushes.Green, Brushes.White, Brushes.Yellow, Brushes.Blue, Brushes.Orange };
+        string[] colorName = {"Red", "Green", "White", "Yellow", "Blue", "Orange"};
+        int[] randomColors = new int[4];
 
         public MainWindow()
         {
             InitializeComponent();     
-            
+            RandomColorCode();
+            ComboBoxItems();
         }
-        
+        private void RandomColorCode ()
+        {         
+            for (int i = 0; i < randomColors.Length; i++)
+            {
+                int index = random.Next(colorCode.Length);
+                randomColors[i] = random.Next(1, 6);
+            }
+
+            this.Title = $"{colorName[randomColors[0]].ToString()}, {colorName[randomColors[1]].ToString()}, {colorName[randomColors[2]].ToString()}, {colorName[randomColors[3]].ToString()}";
+
+        }
 
     }
 }
